9b6c74a179fc75415367d492d27f394f2dc6a8de
Przemo <przemo06133@gmail.com>
2023-12-11 21:23:49 +0100

    Add deploy script

---
+++ script/DeployTaxContract.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {TaxContract} from "../src/TaxContract.sol";
+
+contract DeployTaxContract is Script {
+    function run() external returns (TaxContract) {
+        vm.startBroadcast();
+        TaxContract taxContract = new TaxContract(msg.sender);
+        vm.stopBroadcast();
+        return taxContract;
+    }
+}
